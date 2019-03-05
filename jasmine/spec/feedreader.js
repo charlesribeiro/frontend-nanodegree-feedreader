b91dc53ab31b0cc3f647aae508de6acd02d159d1
Charles <charles.r.ribeiro@gmail.com>
2019-03-05 20:07:28 -0300

    Commit com testes dos RSS feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,15 +32,37 @@ $(function() {
          * and that the URL is not empty.
          */
 
+        it('are URLs defined and not empty', function() {
+
+            for (let feed of allFeeds)
+            {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            }
+            
+        });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it('are names defined and not empty', function() {
+
+            for (let feed of allFeeds)
+            {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+            
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -54,7 +76,11 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
 
+        });
+
+
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -63,10 +89,16 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+    });
+
+
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+    });
 }());
